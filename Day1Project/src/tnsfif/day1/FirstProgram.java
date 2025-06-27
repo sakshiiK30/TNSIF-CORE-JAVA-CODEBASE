e7b8230e1039b216306471d9948178ba96140965
Sakshi <Sakshi@LAPTOP-0J7J6Q7E>
2025-06-27 15:24:37 +0530

    day1

---
+++ Day1Project/src/tnsfif/day1/FirstProgram.java
@@ -0,0 +1,14 @@
+package tnsfif.day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("hello sakshi! ");
+		System.out.println("hello neha! ");
+		
+
+	}
+
+}
