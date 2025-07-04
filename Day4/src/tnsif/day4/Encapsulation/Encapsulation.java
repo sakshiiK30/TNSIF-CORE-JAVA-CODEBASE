8ab613e14e5c907341dee2de9b04b95fb8d90934
Sakshi <Sakshi@LAPTOP-0J7J6Q7E>
2025-07-04 10:06:53 +0530

    day4

---
+++ Day4/src/tnsif/day4/Encapsulation/Encapsulation.java
@@ -0,0 +1,22 @@
+package tnsif.day4.Encapsulation;
+
+public class Encapsulation {
+
+	
+
+		
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			OopsConcept obj=new OopsConcept();
+			
+			obj.setName("Shubham");
+			obj.setAge(23);
+			obj.setSerailNum(1001);
+			
+			System.out.println(obj);
+		}
+
+
+	}
+
